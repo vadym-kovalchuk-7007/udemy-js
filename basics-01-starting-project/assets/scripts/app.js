9e95ea8dce816f2efc944303181dba71a6add59b
TeeZ0NE <teez0ne.flyi4@gmail.com>
2020-09-28 17:09:55 +0300

    create main methods for calculator

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,13 +1,39 @@
 "use strict";
-console.log("Hello from TS");
 var currentResult = 0;
-function add() {
-    if (userInput) {
-        currentResult += parseInt(userInput.value);
+function operations(operator) {
+    var userInp = getUserInput();
+    var prevResult = currentResult;
+    switch (operator) {
+        case "+":
+            currentResult += userInp;
+            break;
+        case "-":
+            currentResult -= userInp;
+            break;
+        case "*":
+            currentResult *= userInp;
+            break;
+        case "/":
+            currentResult /= userInp;
+            break;
     }
+    var message = getMessage(operator, prevResult, userInp, "My message");
+    outputResult(currentResult.toString(), message);
 }
-addBtn === null || addBtn === void 0 ? void 0 : addBtn.addEventListener("click", function () {
-    console.log(userInput.value);
-    console.log(currentResult);
-});
-outputResult(currentResult.toString(), "Current result");
+function getUserInput() {
+    return userInput ? parseInt(userInput.value) : 0;
+}
+function getMessage(operator, prevResult, userInp, message) {
+    if (message === void 0) { message = ""; }
+    return prevResult + " " + operator + " " + userInp + " " + message;
+}
+function add() {
+    operations("+");
+}
+function substract() {
+    operations("-");
+}
+addBtn === null || addBtn === void 0 ? void 0 : addBtn.addEventListener("click", add);
+subtractBtn === null || subtractBtn === void 0 ? void 0 : subtractBtn.addEventListener("click", substract);
+divideBtn === null || divideBtn === void 0 ? void 0 : divideBtn.addEventListener("click", function () { return operations("/"); });
+multiplyBtn === null || multiplyBtn === void 0 ? void 0 : multiplyBtn.addEventListener("click", function () { return operations("*"); });
--- basics-01-starting-project/assets/ts/app.ts
+++ basics-01-starting-project/assets/ts/app.ts
@@ -1,12 +1,73 @@
-console.log("Hello from TS");
 let currentResult: number = 0;
-function add(): void {
-  if (userInput) {
-    currentResult += parseInt(userInput.value);
+
+/**
+ * All operators in one place.
+ * @param operator Divide, substract, etc.
+ */
+function operations(operator: string): void {
+  const userInp: number = getUserInput();
+  const prevResult: number = currentResult;
+  switch (operator) {
+    case "+":
+      currentResult += userInp;
+      break;
+    case "-":
+      currentResult -= userInp;
+      break;
+    case "*":
+      currentResult *= userInp;
+      break;
+    case "/":
+      currentResult /= userInp;
+      break;
   }
+  const message: string = getMessage(
+    operator,
+    prevResult,
+    userInp,
+    "My message"
+  );
+  outputResult(currentResult.toString(), message);
 }
-addBtn?.addEventListener("click", () => {
-  console.log(userInput.value);
-  console.log(currentResult);
-});
-outputResult(currentResult.toString(), "Current result");
+
+/**
+ * Get and check user input.
+ */
+function getUserInput(): number {
+  return userInput ? parseInt(userInput.value) : 0;
+}
+
+/**
+ * Combine message for user.
+ *
+ * @param operator
+ * @param prevResult
+ * @param userInp
+ * @param message
+ */
+function getMessage(
+  operator: string,
+  prevResult: number,
+  userInp: number,
+  message: string = ""
+): string {
+  return `${prevResult} ${operator} ${userInp} ${message}`;
+}
+
+/**
+ * Add operation.
+ */
+function add(): void {
+  operations("+");
+}
+/**
+ * Substract operation.
+ */
+function substract(): void {
+  operations("-");
+}
+/* Event listeners */
+addBtn?.addEventListener("click", add);
+subtractBtn?.addEventListener("click", substract);
+divideBtn?.addEventListener("click", () => operations("/"));
+multiplyBtn?.addEventListener("click", () => operations("*"));
