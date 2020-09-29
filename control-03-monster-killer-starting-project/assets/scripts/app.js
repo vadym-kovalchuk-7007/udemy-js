cb96b1b28b5edd71d50877f6ef59dc34ba4fb4b1
TeeZ0NE <teez0ne.flyi4@gmail.com>
2020-09-30 01:42:08 +0300

    monsterBattle

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,2 +1,92 @@
 "use strict";
 console.log("Moster fighting");
+var ATTACK_VALUE = 10;
+var BONUS_LIFE_COEFFICIENT = 0.2;
+var DEFAULT_MAX_LIFE_VALUE = 100;
+var HARD_ATTACK_VALUE = 17;
+var HEAL_VALUE = 20;
+var MONSTER_ATTACK_VALUE = 14;
+var chosenMaxLife = userInputMaxLife(prompt("Set max life", "" + DEFAULT_MAX_LIFE_VALUE));
+var currentMosterHealth = chosenMaxLife;
+var currentPlayerHealth = chosenMaxLife;
+var hasBonusLife = true;
+var battleLog = [];
+adjustHealthBars(chosenMaxLife);
+attackBtn === null || attackBtn === void 0 ? void 0 : attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn === null || strongAttackBtn === void 0 ? void 0 : strongAttackBtn.addEventListener("click", hardAttackHandler);
+healBtn === null || healBtn === void 0 ? void 0 : healBtn.addEventListener("click", healHandler);
+logBtn === null || logBtn === void 0 ? void 0 : logBtn.addEventListener("click", showBattleLog);
+function userInputMaxLife(userInputValue) {
+    var value;
+    return userInputValue && (value = parseInt(userInputValue))
+        ? Math.abs(value)
+        : DEFAULT_MAX_LIFE_VALUE;
+}
+function attackHandler() {
+    attack(ATTACK_VALUE);
+}
+function hardAttackHandler() {
+    attack(HARD_ATTACK_VALUE);
+}
+function attack(attackValue) {
+    var damageMonster = dealMonsterDamage(attackValue);
+    currentMosterHealth -= damageMonster;
+    var damagePlayer = dealPlayerDamage(attackValue);
+    currentPlayerHealth -= damagePlayer;
+    var data2log = {
+        attackValue: attackValue,
+        damageMonsterValue: damageMonster,
+        damagePlayerValue: damagePlayer,
+        currentMonsterHealth: currentMosterHealth,
+        currentPlayerHealth: currentPlayerHealth,
+        hasBonusLife: hasBonusLife,
+    };
+    writeLog(data2log);
+    checkWinner();
+}
+function checkWinner() {
+    if (currentPlayerHealth <= 0 && hasBonusLife) {
+        hasBonusLife = false;
+        removeBonusLife();
+        var increaseLifeValue = chosenMaxLife * BONUS_LIFE_COEFFICIENT;
+        currentPlayerHealth = increaseLifeValue;
+        increasePlayerHealth(increaseLifeValue);
+    }
+    if (currentMosterHealth <= 0 && currentPlayerHealth > 0) {
+        console.log("You won!");
+    }
+    else if (currentPlayerHealth <= 0 && currentMosterHealth > 0) {
+        console.log("Monster won!");
+    }
+    else if (currentMosterHealth <= 0 && currentPlayerHealth <= 0) {
+        alert("You have a draw");
+    }
+    if (currentMosterHealth <= 0 || currentPlayerHealth <= 0) {
+        reset();
+    }
+}
+function healHandler() {
+    var healValue = currentPlayerHealth >= chosenMaxLife - HEAL_VALUE
+        ? chosenMaxLife - currentPlayerHealth
+        : HEAL_VALUE;
+    increasePlayerHealth(healValue);
+    var data2Log = {
+        healValue: healValue,
+        currentPlayerHealth: currentPlayerHealth += healValue,
+        prevCurrentPlayerHealth: currentPlayerHealth - healValue,
+        currentMosterHealth: currentMosterHealth,
+    };
+    writeLog(data2Log);
+    checkWinner();
+}
+function reset() {
+    resetGame(chosenMaxLife);
+    currentMosterHealth = chosenMaxLife;
+    currentPlayerHealth = chosenMaxLife;
+}
+function writeLog(data) {
+    battleLog.push(data);
+}
+function showBattleLog() {
+    console.log("battleLog:>> ", battleLog);
+}
--- control-03-monster-killer-starting-project/dev/ts/app.ts
+++ control-03-monster-killer-starting-project/dev/ts/app.ts
@@ -1,1 +1,138 @@
 console.log("Moster fighting");
+const ATTACK_VALUE = 10;
+const BONUS_LIFE_COEFFICIENT = 0.2;
+const DEFAULT_MAX_LIFE_VALUE = 100;
+const HARD_ATTACK_VALUE = 17;
+const HEAL_VALUE = 20;
+const MONSTER_ATTACK_VALUE = 14;
+let chosenMaxLife = userInputMaxLife(
+  prompt("Set max life", `${DEFAULT_MAX_LIFE_VALUE}`)
+);
+let currentMosterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
+let battleLog: object[] = [];
+type battleLog = {
+  attackValue: number;
+  damageMonsterValue: number;
+  damagePlayerValue: number;
+  currentPlayerHealth: number;
+  currentMonsterHealth: number;
+  hasBonusLife: boolean;
+};
+type healLog = {
+  healValue: number;
+  currentPlayerHealth: number;
+  prevCurrentPlayerHealth: number;
+  currentMosterHealth: number;
+};
+adjustHealthBars(chosenMaxLife);
+
+attackBtn?.addEventListener("click", attackHandler);
+strongAttackBtn?.addEventListener("click", hardAttackHandler);
+healBtn?.addEventListener("click", healHandler);
+logBtn?.addEventListener("click", showBattleLog);
+
+/**
+ * Validate user input max life value.
+ */
+function userInputMaxLife(userInputValue: string | null): number {
+  let value: number;
+
+  return userInputValue && (value = parseInt(userInputValue))
+    ? Math.abs(value)
+    : DEFAULT_MAX_LIFE_VALUE;
+}
+/**
+ * Attack monster and give monster's feedback.
+ */
+function attackHandler(): void {
+  attack(ATTACK_VALUE);
+}
+
+/**
+ * Hard attack monster and give feedback from him.
+ */
+function hardAttackHandler(): void {
+  attack(HARD_ATTACK_VALUE);
+}
+
+/**
+ * Attack of players.
+ * @param attackValue
+ */
+function attack(attackValue: number): void {
+  const damageMonster: number = dealMonsterDamage(attackValue);
+  currentMosterHealth -= damageMonster;
+  const damagePlayer: number = dealPlayerDamage(attackValue);
+  currentPlayerHealth -= damagePlayer;
+  let data2log: battleLog = {
+    attackValue: attackValue,
+    damageMonsterValue: damageMonster,
+    damagePlayerValue: damagePlayer,
+    currentMonsterHealth: currentMosterHealth,
+    currentPlayerHealth: currentPlayerHealth,
+    hasBonusLife: hasBonusLife,
+  };
+  writeLog(data2log);
+  checkWinner();
+}
+/**
+ * Check who is winner.
+ */
+function checkWinner(): void {
+  // Player has bonus life
+  if (currentPlayerHealth <= 0 && hasBonusLife) {
+    hasBonusLife = false;
+    removeBonusLife();
+    let increaseLifeValue = chosenMaxLife * BONUS_LIFE_COEFFICIENT;
+    currentPlayerHealth = increaseLifeValue;
+    increasePlayerHealth(increaseLifeValue);
+  }
+  if (currentMosterHealth <= 0 && currentPlayerHealth > 0) {
+    console.log("You won!");
+  } else if (currentPlayerHealth <= 0 && currentMosterHealth > 0) {
+    console.log("Monster won!");
+  } else if (currentMosterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert("You have a draw");
+  }
+  if (currentMosterHealth <= 0 || currentPlayerHealth <= 0) {
+    reset();
+  }
+}
+
+/**
+ * Heal of player.
+ */
+function healHandler(): void {
+  let healValue: number =
+    currentPlayerHealth >= chosenMaxLife - HEAL_VALUE
+      ? chosenMaxLife - currentPlayerHealth
+      : HEAL_VALUE;
+  increasePlayerHealth(healValue);
+  let data2Log: healLog = {
+    healValue: healValue,
+    currentPlayerHealth: currentPlayerHealth += healValue,
+    prevCurrentPlayerHealth: currentPlayerHealth - healValue,
+    currentMosterHealth: currentMosterHealth,
+  };
+  writeLog(data2Log);
+  checkWinner();
+}
+
+/**
+ * Set health to max.
+ */
+function reset(): void {
+  resetGame(chosenMaxLife);
+  currentMosterHealth = chosenMaxLife;
+  currentPlayerHealth = chosenMaxLife;
+}
+
+function writeLog(data: battleLog | healLog): void {
+  battleLog.push(data);
+}
+
+function showBattleLog(): void {
+  console.log("battleLog:>> ", battleLog);
+}
