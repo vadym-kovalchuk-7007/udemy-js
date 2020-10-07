04408e8c263432b5bb9ed99ff836b89e8ae631e9
TeeZ0NE <teez0ne.flyi4@gmail.com>
2020-10-07 21:50:45 +0300

    nodejs start

--- nodeJs/app.ts
+++ nodeJs/app.ts
@@ -1,19 +1,9 @@
-import * as http from "http";
+import express = require("express");
+import bodyParser = require("body-parser");
+const app = express();
 
-const server = http.createServer((request, response) => {
-  let body: Buffer[] = [];
-  let body2Sting: string;
-  request.on("data", (chunk) => {
-    console.log(chunk);
-    body.push(chunk);
-  });
-  request.on("end", () => {
-    body2Sting = Buffer.concat(body).toString();
-  });
-  response.setHeader("Content-type", "text/html");
-  response.write(
-    '<h2>First local server</h2><form method="POST" action="/"><input type="text" name="user-name"/><button type="submit">Send<?button></form>'
-  );
-  response.end();
+app.use(bodyParser.json());
+app.get("/", function (req, res) {
+  res.send("hello world");
 });
-server.listen(3030);
+app.listen(3030);
